6fbb6e322aa9fb3220a2219ea4332accf8ee1b0f
andresg <Andres Gomez>
2022-04-09 05:00:33 -0500

    upload classic programming excercise with its solutions

---
+++ classics/caesar_cipher.rb
@@ -0,0 +1,16 @@
+def caesar_cipher(string, shift)
+	diff = 'z'.ord - 'a'.ord + 1
+
+	new_string =	string.split("").map do |char|
+		if char.between?('a', 'z')
+	 		((char.ord - 'a'.ord + shift) % diff + 'a'.ord).chr			
+		elsif char.between?('A', 'Z')
+			((char.ord - 'A'.ord + shift) % diff + 'A'.ord).chr			
+		else
+			char
+	 	end
+	end
+	new_string.join
+end
+
+p caesar_cipher("What a string!", 5)
