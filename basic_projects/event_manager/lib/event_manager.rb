ad118f20862c4f03afd61b055736166c4580511a
andresg <Andres Gomez>
2022-04-21 18:31:17 -0500

    clean number module and find peak days and hours module

--- basic_projects/event_manager/lib/event_manager.rb
+++ basic_projects/event_manager/lib/event_manager.rb
@@ -22,7 +22,64 @@ def legislators_by_zipcode(zipcode)
   end
 end
 
+def clean_homephone(homephone)
+  homephone = homephone.gsub(/-|\(|\)|\.| /, '')
+
+  if homephone.length < 10
+    'wrong number'
+  elsif homephone.length == 10
+    homephone
+  elsif homephone.length == 11
+    if homephone[0] == '1'
+      homephone[1..11]
+    else
+      'wrong number'
+    end
+  end
+
+end
+
+def clean_regdate(regdate)
+  date = DateTime.strptime(regdate, '%Y/%d/%m %H:%M')
+end
+
+def save_thank_you_letter(id,form_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+end
+
 puts 'Event Manager Initialized!'
+
+def create_thanks_htmls()
+  file_name = "event_attendees.csv"
+  contents = CSV.open(
+    file_name, 
+    headers: true,  
+    header_converters: :symbol)
+  
+  template_letter = File.read_safe('form_letter.erb')
+  erb_template = ERB.new template_letter
+  
+  contents.each do |row|
+    id = row[0]
+    name = row[:first_name]
+    zipcode = clean_zipcode(row[:zipcode])
+    homephone = clean_homephone(row[:homephone])
+    regdate = clean_regdate(row[:regdate])
+  
+    legislators = legislators_by_zipcode(zipcode)
+  
+    form_letter = erb_template.result(binding)
+  
+    save_thank_you_letter(id, form_letter)
+  end
+end
+
 
 file_name = "event_attendees.csv"
 contents = CSV.open(
@@ -30,16 +87,14 @@ contents = CSV.open(
   headers: true,  
   header_converters: :symbol)
 
+dates = contents.each.map {|row| clean_regdate(row[:regdate]) } 
 
-template_letter = File.read_safe('form_letter.erb')
-erb_template = ERB.new template_letter
+hours = dates.map { |date| date.hour }.tally 
+peak_hours = hours.sort_by{|k, v| v * -1}.transpose[0]
+puts "Peak hours in order: #{peak_hours}"
 
-contents.each do |row|
-  name = row[:first_name]
-  zipcode = clean_zipcode(row[:zipcode])
+days = dates.map {|date| date.wday}.tally
+peak_days = days.sort_by{|k, v| v * -1}.transpose[0]
+days = ["Sunday", "Monday", "Tuesday", "Wendsday", "Thursday", "Friday", "Saturday"]
 
-  legislators = legislators_by_zipcode(zipcode)
-
-  form_letter = erb_template.result(binding)
-  puts form_letter
-end
+puts "Peak days in order: #{peak_days.map { |day_i| days[day_i] } }"
