13ca10377297dcc45cc01947e83d7156a596fa01
andresg <Andres Gomez>
2022-04-22 22:18:14 -0500

    feat: adding functionalty to save game and upload it again

--- basic_projects/hangman/hangman.rb
+++ basic_projects/hangman/hangman.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require 'json'
 
 # User class to save people playing
 class User
@@ -6,9 +7,9 @@ class User
   # This is a basic user class
   attr_accessor :name, :score
 
-  def initialize(name)
+  def initialize(name, score = 0)
     @name = name
-    @score = 0
+    @score = score
   end
 end
 
@@ -38,6 +39,12 @@ module BasicUtils
     answer = gets_message('Do you want to play another round? (y/n)')
     answer = gets_message('Please enter a valid option: (y/n)') while answer != 'y' && answer != 'n'
     answer == 'n'
+  end
+
+  def load_game?
+    answer = gets_message('Do you want to load your user info? (y/n)')
+    answer = gets_message('Please enter a valid option: (y/n)') while answer != 'y' && answer != 'n'
+    answer == 'y'
   end
 end
 
@@ -126,14 +133,22 @@ end
 class Hangman < HangManDraw
   include BasicUtils
 
-  def initialize(word_file = 'google-10000-english-no-swears.txt')
+  def initialize(filename = 'google-10000-english-no-swears.txt')
     super()
-    @word_file = File.open(word_file)
+    standart_init(filename)
+  end
+
+  def standart_init(filename = 'google-10000-english-no-swears.txt')
+    @filename = filename
+    @word_file = File.open(@filename)
     @n_lines = @word_file.count
     @round_word = select_random_word
     @guessed_word = '_' * @round_word.length
     @users = []
     @rounds = 0
+    @characters = ('a'..'z').to_a
+    @characters_chosen = Array.new(@characters.length, '_')
+    @save = false
   end
 
   def select_random_word
@@ -145,13 +160,21 @@ class Hangman < HangManDraw
   def draw_word_picker
     to_print_characters = @guessed_word.split('')
     to_print_word = to_print_characters.reduce('') { |to_print, char| to_print + "#{char} " }
-    puts "\n#{to_print_word}\n\n"
+    to_print_word = "\n#{to_print_word}\n"
+    @characters.each_with_index do |c, i|
+      to_print_word += (i % 6).zero? ? "\n#{c} #{@characters_chosen[i]}   " : "#{c} #{@characters_chosen[i]}   "
+    end
+    puts to_print_word
   end
 
   def draw_final_mesage
     match_condition = @guessed_word == @round_word
     puts 'Congrats!! You have won!' if match_condition
     puts "Try again! The correct word was #{@round_word}" if !match_condition && @wrong_number == @max_wrong
+  end
+
+  def draw_save_message
+    puts "Thanks for saving #{@users[0].name}, see you again soon" if @save
   end
 
   def draw_board
@@ -161,31 +184,46 @@ class Hangman < HangManDraw
   end
 
   def enter_name
-    name = "andres" # gets_message('Plase enter the name of the player to play')
+    name = gets_message('Plase enter the name of the player to play')
     @users.push(User.new(name))
   end
 
   def update_game_state(char)
-    matches = @round_word.indices(char)
-    matches.each { |match_i| @guessed_word[match_i] = char }
-    @wrong_number += 1 if matches.length.zero?
-    @users[0].score += 1 if @guessed_word == @round_word
+    if char != 'save'
+      matches = @round_word.indices(char)
+      matches.each { |match_i| @guessed_word[match_i] = char }
+      @wrong_number += 1 if matches.length.zero?
+      @users[0].score += 1 if @guessed_word == @round_word
+      index_char = @characters.join.index(char)
+      @characters_chosen[index_char] = matches.length.positive? ? 'o' : 'x'
+    else
+      @save = true
+    end
   end
 
   def guessed_char
-    char = gets_message('Please guess a character for the above spaces')
-    char = gets_message('Please select a valid character') until char.match?(/^[a-z]$/) || char.match?(/^[A-Z]$/)
+    char = gets_message("\nPlease guess a character for the above spaces or type save").downcase
+    char = gets_message('Please select a valid character').downcase until char.match?(/^[a-z]$/) || char == 'save'
     char
+  end
+
+  def save_game
+    if @save
+      draw_save_message
+      save_to_json
+      exit
+    end
   end
 
   def play_round
     display_score('partial')
     while @wrong_number < @max_wrong
       draw_board
-      char = 'x' # guessed_char
+      char = guessed_char
       update_game_state(char)
-      break if @guessed_word == @round_word
+      break if @guessed_word == @round_word || @save
     end
+    save_game
     draw_board
     @rounds += 1
   end
@@ -205,6 +243,7 @@ class Hangman < HangManDraw
     @round_word = select_random_word
     @guessed_word = '_' * @round_word.length
     @wrong_number = 0
+    @characters_chosen = Array.new(@characters.length, '_')
   end
 
   def end_game
@@ -217,13 +256,63 @@ class Hangman < HangManDraw
     puts "    win rate (#{@users[0].score}/#{@rounds}): #{@users[0].score} wins in #{@rounds} rounds\n\n"
   end
 
+  def load_game
+    try_times = 0
+    begin
+      init_from_json("output/hangman_#{@users[0].name}.json")
+    rescue TypeError
+      puts e
+      @users[0].name = gets_message('Please enter your user name again to find the file')
+      try_times += 1
+      retry
+    end
+  end
+
   def prepare_game
     enter_name
+    load_game if load_game?
   end
 
   def play_game
     prepare_game
     play_recursive
+  end
+
+  def to_json(*_args)
+    JSON.dump ( {
+      json_class: self.class.name,
+      data: {
+        filename: @filename, round_word: @round_word,
+        guessed_word: @guessed_word, users: { name: @users[0].name, score: @users[0].score },
+        rounds: @rounds, wrong_number: @wrong_number,
+        characters_chosen: @characters_chosen
+      }
+    })
+  end
+
+  def save_to_json
+    Dir.mkdir('output') unless Dir.exist?('output')
+    File.open("output/hangman_#{@users[0].name}.json", 'w') do |f|
+      f.write(to_json)
+    end
+  end
+
+  def fetch_state_game(data_json)
+    @wrong_number = data_json['data']['wrong_number']
+    @round_word = data_json['data']['round_word']
+    @guessed_word = data_json['data']['guessed_word']
+    @rounds = data_json['data']['rounds']
+  end
+
+  def fetch_user_info(data_json)
+    @users.push(User.new(data_json['data']['users']['name'], data_json['data']['users']['score']))
+  end
+
+  def init_from_json(json_filename)
+    data_json = JSON.parse(File.read(json_filename))
+    standart_init(data_json['data']['filename'])
+    fetch_state_game(data_json)
+    fetch_user_info(data_json)
   end
 end
 
