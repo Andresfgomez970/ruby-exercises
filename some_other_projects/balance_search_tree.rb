0ded379958ddc0e457f7eb8e206aef6bd0708941
andresg <Andres Gomez>
2022-05-06 16:06:19 -0500

    minor fix of format in ruby

--- some_other_projects/balance_search_tree.rb
+++ some_other_projects/balance_search_tree.rb
@@ -262,7 +262,7 @@ class Tree
     @root = build_tree(data)
   end
 
-  def pretty_print(node = @root, prefix = '', is_left = true)
+  def pretty_print(node = @root, prefix = '', is_left: true)
     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
     pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
