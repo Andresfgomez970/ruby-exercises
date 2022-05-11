e2b97ce560314f358724ccaea021fd9ad89db331
andresg <Andres Gomez>
2022-05-10 20:08:37 -0500

    adding tests for caesar cipher

--- classics/spec/caesar_cipher_spec.rb
+++ classics/spec/caesar_cipher_spec.rb
@@ -2,6 +2,14 @@ require './caesar_cipher'
 
 describe '#caesar_cipher' do
   it 'simple shift' do
-    expect(caesar_cipher('abc', 1)).to eq('bcd')
+    expect(caesar_cipher('abcAB', 1)).to eq('bcdBC')
+  end
+
+  it 'module shift' do
+    expect(caesar_cipher('ZAm', 4)).to eq('DEq')
+  end
+
+  it 'ignore special characters' do
+    expect(caesar_cipher('¿Z,A!m#', 4)).to eq('¿D,E!q#')
   end
 end
