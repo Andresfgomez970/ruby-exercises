927038c3e33f3a2e5481e6a8b270de4fa36d786c
andresg <Andres Gomez>
2022-05-15 14:32:59 -0500

    correct standard of rubocop

--- some_games/chess/chessgame.rb
+++ some_games/chess/chessgame.rb
@@ -7,7 +7,7 @@ require_relative 'utils'
 # Ches class for the game
 class ChessGame
   include BasicUtils
-  
+
   def initialize(player1 = ChessGameUser.new(), player2 = ChessGameUser.new(), table = Table.new)
     @player1 = player1
     @player2 = player2
