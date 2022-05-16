36e0d3c36ebf9c9914f55d7e7a69f36956c604ea
andresg <Andres Gomez>
2022-05-15 21:31:38 -0500

    add all symbols of chess in uniode points to be shown in the board game

--- some_games/chess/pieces.rb
+++ some_games/chess/pieces.rb
@@ -1,6 +1,25 @@
-class Piece 
-  def initialize(name = nil, symbol = nil)
-    @name = name
+# frozen_string_literal: true
+
+# Constants for initialization of pieces
+WHITE_KING = "\u265A"
+WHITE_QUEEN = "\u265B"
+WHITE_ROOK = "\u265C"
+WHITE_BISHOP = "\u265D"
+WHITE_KNIGHT = "\u265E"
+WHITE_PAWN = "\u265F"
+
+BLACK_KING = "\u2654"
+BLACK_QUEEN = "\u2655"
+BLACK_ROOK = "\u2656"
+BLACK_BISHOP = "\u2657"
+BLACK_KNIGHT = "\u2658"
+BLACK_PAWN = "\u2659"
+
+# class like this?
+class Piece
+  def initialize(initial = nil, symbol = nil)
+    @initial = initial
     @symbol = symbol
   end
 end
+
