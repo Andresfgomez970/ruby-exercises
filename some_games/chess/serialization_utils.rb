b7631c23caec9975ad855058c1bce99777fc10f3
andresg <Andres Gomez>
2022-05-20 14:31:22 -0500

    add general serialization for objects that have classes

--- some_games/chess/serialization_utils.rb
+++ some_games/chess/serialization_utils.rb
@@ -7,20 +7,41 @@ module BasicSerializable
   #class (e.g. MessagePack, JSON, YAML) to get a different
   #serialization
   @@serializer = JSON
+  @@default_classes = [Integer, Float, String, Array, Hash, NilClass, Symbol]
+  @@default_classes_name = ['Integer', 'Float', 'String', 'Array', 'Hash', 'NilClass', 'Symbol']
 
-  def serialize
+
+  def serialize(to_serialize = self)
     obj = {}
-    instance_variables.map do |var|
-      obj[var] = instance_variable_get(var)
+
+    to_serialize.instance_variables.map do |var|
+      class_of_var = to_serialize.instance_variable_get(var).class
+      obj[var] = {}
+      obj[var]['class_name'] = class_of_var
+
+      if @@default_classes.include?(class_of_var)
+        obj[var]['data'] = to_serialize.instance_variable_get(var)
+      else
+        obj[var]['data'] = serialize(to_serialize.instance_variable_get(var))
+      end
     end
 
     @@serializer.dump obj
   end
 
-  def unserialize(string)
+  def unserialize(string, to_unserialize = self)
     obj = @@serializer.parse(string)
+    
     obj.keys.each do |key|
-      instance_variable_set(key, obj[key])
+      class_of_var = obj[key]['class_name']
+      if @@default_classes_name.include?(class_of_var)
+        data = obj[key]['data']
+        to_unserialize.instance_variable_set(key, data)
+      else
+        to_unserialize.instance_variable_set(key, Kernel.const_get(class_of_var).new)
+        unserialize(obj[key]['data'], to_unserialize.instance_variable_get(key))
+      end
     end
+  
   end
 end
